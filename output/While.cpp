9b22e9867d2e70b70a488c7346f9b17dd9ed9623
Sulthonhanan <hananakhnaf31@gmail.com>
2025-03-18 11:31:25 +0700

    while baru

--- output/While.cpp
+++ output/While.cpp
@@ -5,20 +5,28 @@ using namespace std;
 
 
 int main(){
-    int x;
-    int While = 0;
-    int DoWhile = 0;
+    int n;
+    int perulanganWhile = 0;
+    int perulanganDoWhile = 0;
 
     //Perulangan While
 
-    cout << "Perulangan dengan while" << endl;
+    while (n < 7)
+    {
+        cout << "Nilai n = " << n << endl;
+        n = rand() % 10;
+        perulanganWhile++;
+    }
+    cout << "Jumlah perulangan = " << perulanganWhile << endl;
+    cout << "Nilai n terakhir = " << n << endl;
+
+    cout << "Perulangan do while" << endl;
+
     srand(time(0));
-    x = rand () % 10;
-    cout << " Nilai Awal X = " << x << endl;
-    while (x < 7)
-        cout << "Nilai X = " << x << "Lari keliling lapangan" << endl;
-        x = rand () % 10;
-         
+
+    n = rand() % 10;
+
+    cout << "Nilai awal n = " << n << endl;
 
 
 }
