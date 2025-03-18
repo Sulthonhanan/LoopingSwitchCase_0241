51df854d813ab26ada74a28fdbf589d5d2059ec7
Sulthonhanan <hananakhnaf31@gmail.com>
2025-03-18 11:02:30 +0700

    menambahkan Nilai pencacah terakhir

--- Looping.cpp
+++ Looping.cpp
@@ -1,12 +1,17 @@
-    #include <iostream>
-    using namespace std;
+#include <iostream>
+using namespace std;
 
-    int main(){
-        int x;
-        //perulangan for untuk pencacah naik
-        for (x = 0; x <= 4; x++;){
-            cout << "Nilai Pencacah = " << x << "Selamat Datang" << endl;
-        } 
-    }
+int main(){
+    int x;
+    //perulangan for untuk pencacah naik
+    for (x = 0; x <= 4; x++;){
+        cout << "Nilai Pencacah = " << x << "Selamat Datang" << endl;
+    } 
+
+    cout << "Nilai pencacah terakhir =  " << endl;
+    cout << endl;
+    cout << endl;
 
     
+}
+
