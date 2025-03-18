8a91166fb766e48869b0767e45b6de24bc34b6f2
Sulthonhanan <hananakhnaf31@gmail.com>
2025-03-18 10:56:28 +0700

    Perulangan cacah naik

---
+++ Looping.cpp
@@ -0,0 +1,12 @@
+    #include <iostream>
+    using namespace std;
+
+    int main(){
+        int x;
+        //perulangan for untuk pencacah naik
+        for (x = 0; x <= 4; x++;){
+            cout << "Nilai Pencacah = " << x << "Selamat Datang" << endl;
+        } 
+    }
+
+    
