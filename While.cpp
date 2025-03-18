763ab6b367daa1c6d7b7b730cd34d6b66d0515ee
Sulthonhanan <hananakhnaf31@gmail.com>
2025-03-18 11:37:01 +0700

    Dowhile

---
+++ While.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+
+int main(){
+    int n;
+    int perulanganWhile = 0;
+    int perulanganDoWhile = 0;
+
+    //Perulangan While
+
+    while (n < 7)
+    {
+        cout << "Nilai n = " << n << endl;
+        n = rand() % 10;
+        perulanganWhile++;
+    }
+    cout << "Jumlah perulangan = " << perulanganWhile << endl;
+    cout << "Nilai n terakhir = " << n << endl;
+
+    cout << "Perulangan do while" << endl;
+
+    srand(time(0));
+
+    n = rand() % 10;
+
+    cout << "Nilai awal n = " << n << endl;
+
+    do
+    {
+        cout << "Nilai n = " << n << endl;
+        n = rand() % 10;
+        perulanganDoWhile++;
+    } while (n < 7);
+    
+    cout << "Jumlah perulangan = " << perulanganWhile << endl;
+    cout << "Nilai n terakhir = " << n << endl;
+
+}
